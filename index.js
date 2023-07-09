2edaefe14fefdebc842b9c6e26a8654781204671
Umar121212 <iskandarov.u12@gmail.com>
2023-07-10 02:23:27 +0500

    Homework17

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/* let students = [];
+let newStudent = prompt("Введите ваше имя студента:");
+
+students.push(newStudent); */
+
+/* alert("Вы последний студент в группе: " + students[students.length - 1]); */
 let students =[]
 let countStudents = Number(prompt('Сколько студентов в группе?'));
 let idStudents = prompt('Введите ваше имя:')
